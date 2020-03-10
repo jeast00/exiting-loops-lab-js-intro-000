4c7e2b8fb54813242e329430ced463f20c08c3cb
Jonathan East <jonathan_east@ymail.com>
2020-03-10 01:50:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function breakOut(array, changeValue, stopValue) {
-
+  array.forEach(changeValue => {
+    if(stopValue){
+      break;
+      return array;
+    }
+  });
 }
 
 function keepGoing(array, changeValue, skipValue) {
