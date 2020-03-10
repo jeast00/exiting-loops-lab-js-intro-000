e71748f3e270ef445e3577c5f44927dcf13d0544
Jonathan East <jonathan_east@ymail.com>
2020-03-10 02:16:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ function breakOut(array, changeValue, stopValue) {
 }
 
 function keepGoing(array, changeValue, skipValue) {
-
+  for (var i = 0; i < array.length; i++) {
+    if(array[i] === skipValue) {
+      continue;
+    }
+  }
 }
 
 function findBy(array, findFn) {
