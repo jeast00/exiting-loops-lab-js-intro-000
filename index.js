b4975541f994336c232cc22011bf584d18525682
Jonathan East <jonathan_east@ymail.com>
2020-03-10 02:08:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function findBy(array, findFn) {
   let findValue = null;
 
   for (var i = 0; i < array.length; i++) {
-    if(array[i] !== findFn) {
+    if(array[i] === findFn) {
       findValue = array[i];
       break;
     }
